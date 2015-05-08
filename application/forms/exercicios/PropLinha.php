0f9d6f6806fb8f3922aa12f726bc15234aa5874d
mdymen <msn@dymenstein.com>
2015-05-08 17:22:23 -0300

    adicionar novo slide gerar codigo

--- application/forms/exercicios/PropLinha.php
+++ application/forms/exercicios/PropLinha.php
@@ -16,16 +16,16 @@ class Forms_Exercicios_PropLinha extends Zend_Form {
     function init() {
         $decorator = new Decorators_Decorator1();
         
-        $id = new Zend_Form_Element_Text('txtId', array('placeholder' => 'ID da pergunta'));
+        $id = new Zend_Form_Element_Text('txtIdLinhas', array('placeholder' => 'ID da pergunta'));
         $id->addDecorator($decorator);
         
-        $izq = new Zend_Form_Element_Text('txtIzq', array('placeholder' => 'Texto da izq...'));
+        $izq = new Zend_Form_Element_Text('txtIzqLinhas', array('placeholder' => 'Texto da izq...'));
         $izq->addDecorator($decorator);
         
-        $dir = new Zend_Form_Element_Text('txtDer', array('placeholder' => 'Texto da dir...'));
+        $dir = new Zend_Form_Element_Text('txtDerLinhas', array('placeholder' => 'Texto da dir...'));
         $dir->addDecorator($decorator);
         
-        $resp = new Zend_Form_Element_Text('txtResp', array('placeholder' => 'Resposta...'));
+        $resp = new Zend_Form_Element_Text('txtRespLinhas', array('placeholder' => 'Resposta...'));
         $resp->addDecorator($decorator);
         
         $this->addElements(array($id, $izq, $dir, $resp));
